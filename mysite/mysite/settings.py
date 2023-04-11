ade4416cc7a36881ea0a6b77b8bfed3661d69036
Kyle Nathan Naranjo <63973445+teslakoile@users.noreply.github.com>
2023-04-11 21:01:01 +0800

    removed procfile

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,3 +149,4 @@ LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = LOGIN_URL
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+django_heroku.settings(locals())
