c747394786b8916b5e53f98871498c1e8034c07a
Kyle Nathan Naranjo <63973445+teslakoile@users.noreply.github.com>
2023-04-22 15:53:07 +0800

    uploaded more questions

--- mysite/models.py
+++ mysite/models.py
@@ -2,16 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Question(models.Model):
+    id = models.IntegerField(primary_key=True)
     passage = models.TextField()
     question_text = models.TextField()
     choices = models.JSONField()
-    answer = models.CharField(max_length=1)
-    knowledge_component = models.CharField(max_length=50)
-    id = models.IntegerField(primary_key=True)
+    answer = models.CharField(max_length=2)
+    knowledge_component = models.CharField(max_length=255)
+    relevant_sentences = models.JSONField(default=list)
+    choices_in_complete_thought = models.JSONField(default=list)
+    dc_score = models.FloatField(default=0.0)
+    difficulty_score = models.FloatField(default=0.0)
 
     def __str__(self):
         return self.question_text
-
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     history = models.TextField(blank=True, default='')
--- mysite/scripts/upload_json.py
+++ mysite/scripts/upload_json.py
@@ -12,18 +12,26 @@ django.setup()
 from mysite.models import Question
 
 # Replace this with the path to your JSON file
-json_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'Compiler', 'aicomprehend_dataset_v9.json')
+json_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'Compiler', 'aicomprehend_annotated_dataset_v7.json')
 
 with open(json_file_path, 'r') as f:
     data = json.load(f)
 
 for item in data:
-    mymodel_instance = Question(
-        passage=item['passage'],
-        question_text=item['question'],
-        choices=item['choices'],
-        answer=item['answer'],
-        knowledge_component=item['knowledge component'],
-        id=item['id']
-    )
-    mymodel_instance.save()
+    try:
+        mymodel_instance = Question()
+        mymodel_instance.passage = item['passage']
+        mymodel_instance.question_text = item['question']
+        mymodel_instance.choices = item['choices']
+        mymodel_instance.answer = item['answer']
+        mymodel_instance.knowledge_component = item['knowledge_component']
+        mymodel_instance.id = item['id']
+        mymodel_instance.relevant_sentences = item['relevant_sentences']
+        mymodel_instance.choices_in_complete_thought = item['choices_in_complete_thought']
+        mymodel_instance.dc_score = item['dc_score']
+        mymodel_instance.difficulty_score = item['difficulty_score']
+        mymodel_instance.save()
+
+    except KeyError as e:
+        print(f"KeyError: {e} in item with id: {item['id']}")
+
